2d4e2bcea100b17c2fe579a95056fec351472494
leon <leonsk32@gmail.com>
2021-05-17 18:21:13 +0900

    add one digit case

--- specs/test.spec.ts
+++ specs/test.spec.ts
@@ -1,7 +1,18 @@
 import { chinese2Arabic } from "../src"
 
 describe("chinese2Arabic", function () {
-  it("For sentences that do not contain numbers, return the original text as is.", () => {
-   expect(chinese2Arabic("こんにちは。")).toBe("こんにちは。")
+  it.each`
+    input | expected
+    ${"一"} | ${"1"}
+    ${"二"} | ${"2"}
+    ${"三"} | ${"3"}
+    ${"四"} | ${"4"}
+    ${"五"} | ${"5"}
+    ${"六"} | ${"6"}
+    ${"七"} | ${"7"}
+    ${"八"} | ${"8"}
+    ${"九"} | ${"9"}
+  `("one digit [$input -> $expected]", ({input, expected}) => {
+    expect(chinese2Arabic(input)).toBe(expected)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export function chinese2Arabic(text: string) {
-  return text
+  return chinese2ArabicMap[text].toString()
+}
+
+const chinese2ArabicMap: Record<string, number> = {
+  "一": 1,
+  "二": 2,
+  "三": 3,
+  "四": 4,
+  "五": 5,
+  "六": 6,
+  "七": 7,
+  "八": 8,
+  "九": 9,
 }
